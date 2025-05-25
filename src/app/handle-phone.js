0db695bf86e47f3a610188547c6751cc66144342
techyVS <victorsky90@gmail.com>
2025-05-25 21:34:15 +0100

    updated the sliders with better heading and subheading copies, as well as fixed the 'fixed-phone' module not found for other pages where it doesn't exist as an element

--- src/app/handle-phone.js
+++ src/app/handle-phone.js
@@ -2,7 +2,9 @@ const fixedPhone = document.querySelector('.positioned-phone');
 
 export const showPhoneOnScroll = () => {};
 document.addEventListener('scroll', () => {
-	if (scrollY >= 400) {
-		fixedPhone.classList.add('show-phone');
-	} else fixedPhone.classList.remove('show-phone');
+	if (fixedPhone) {
+		if (scrollY >= 400) {
+			fixedPhone.classList.add('show-phone');
+		} else fixedPhone.classList.remove('show-phone');
+	} else return;
 });
